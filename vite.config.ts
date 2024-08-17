7e088593aa199c491c95406549a021b1eab9d7c1
hoinlee-moi <lhi3048@gmail.com>
2024-08-18 03:13:27 +0900

    css library change vanilla extract -> tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), vanillaExtractPlugin()],
+  plugins: [react()],
 });
