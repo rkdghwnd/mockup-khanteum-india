a788c4081ecc1eecf88a51a8a2b8b877bc4c9748
hoinlee-moi <lhi3048@gmail.com>
2024-09-03 15:51:57 +0900

    login & upload

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,10 +3,10 @@ import SocialLogin from "../components/Login/SocialLogin";
 
 const Login = () => {
   const LoginHandle = ({ id, pw }: { id: string; pw: string }) => {
-    console.log(id, pw);
+    let ad = 2;
   };
   return (
-    <section className="bg-[url('bg_pc.jpg')] h-[calc(100vh-105px)] overflow-hidden flex flex-col items-center justify-center">
+    <section className="bg-[url('bg_pc.png')] h-[calc(100vh-105px)] overflow-hidden flex flex-col items-center justify-center">
       <img src="logo.png" className="w-[56px] h-[56px]" />
       <LoginForm formAction={LoginHandle} />
       <SocialLogin />
--- src/pages/Upload.tsx
+++ src/pages/Upload.tsx
@@ -6,7 +6,7 @@ const Upload = () => {
   return (
     <section className="min-h-[calc(100vh-105px)] flex flex-col items-center">
       <h1 className="text-center pt-2 font-bold text-xl text-[#464646]">Upload Video</h1>
-      <div className="bg-[url('bg_pc.jpg')] h-[calc(100vh-157px)] w-[calc(100%-20px)] mt-4 px-5 rounded-3xl shadow-[0px_-2px_12px_1px_rgba(2,69,149,0.25)] flex flex-col items-center">
+      <div className="bg-[url('bg_pc.png')] bg-center bg-cover h-[calc(100vh-157px)] w-[calc(100%-20px)] mt-4 px-5 rounded-3xl shadow-[0px_-2px_12px_1px_rgba(2,69,149,0.25)] flex flex-col items-center">
         <UploadProfile />
         <SelectCategory selectList={CATEGORY} />
       </div>
