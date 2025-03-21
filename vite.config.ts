f8924a588eab4f3e034b36c852fc4064225aa1f5
rkdghwnd <ghwnd6448@gmail.com>
2025-03-21 16:47:15 +0900

    etc: config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,11 @@ export default defineConfig({
   plugins: [react()],
   define: {
     global: {},
+    build: {
+      commonjsOptions: {
+        strictRequires: ["node_modules/aws-sdk/**/*.js"],
+      },
+    },
   },
   server: {
     port: 3000, // 포트번호 3000으로 변경
